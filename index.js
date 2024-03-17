ec3a760592b7cc92f8bbb7ff43e8852fa3c392a5
Guru Code <codeguru28@gmail.com>
2024-03-17 11:25:35 +0530

    Ratings & Contact-us Section Completed

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     const ham = document.querySelector('.ham');
+    const body = document.querySelector('body');
+    const noScroll = document.querySelector('.no-scroll');
     const hamClose = document.querySelector('.close');
     const mobileNavLinks = document.querySelectorAll('.mobile-nav .links');
 
     function closeMobileNav() {
         const mobileNav = document.querySelector('.mobile-nav');
+        body.classList.remove('no-scroll');
         mobileNav.classList.remove('mobile-nav-open');
     }
 
@@ -19,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ham.addEventListener('click', () => {
         const mobileNav = document.querySelector('.mobile-nav');
         mobileNav.classList.add('mobile-nav-open');
+        body.classList.add('no-scroll');
     });
 
     hamClose.addEventListener('click', closeMobileNav);
@@ -26,4 +30,29 @@ document.addEventListener('DOMContentLoaded', function () {
     mobileNavLinks.forEach(link => {
         link.addEventListener('click', handleLinkClick);
     });
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-next-button",
+          prevEl: ".swiper-prev-button",
+        },
+        breakpoints: {
+          320:{
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        },
+      });
+
 });
